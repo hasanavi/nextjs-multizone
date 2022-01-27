a27c4d0ccb3b1adb0c26e37357141435cc3e58d4
Hasan Haque <hasanavi@gmail.com>
2022-01-27 16:43:26 +0000

    staging 2

--- home/pages/index.js
+++ home/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
 	return (
 		<div>
 			<Header />
-			<p>This is our homepage Yahoo - Staging 1</p>
+			<p>This is our homepage Yahoo - Staging 2</p>
 			<div>
 				<a href="/blog">Blog</a>
 			</div>
