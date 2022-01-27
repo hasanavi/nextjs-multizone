b8e5e58e59aaea15d457d4dfeb666e0bd12c5571
Hasan Haque <hasanavi@gmail.com>
2022-01-27 16:41:30 +0000

    staging update

--- blog/pages/post/[id].js
+++ blog/pages/post/[id].js
@@ -1,15 +1,15 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Post() {
-  const router = useRouter()
-  return (
-    <div>
-      <h3>Post #{router.query.id}</h3>
-      <p>Lorem ipsum</p>
-      <Link href="/">
-        <a>Back to blog</a>
-      </Link>
-    </div>
-  )
+	const router = useRouter();
+	return (
+		<div>
+			<h3>Updated Post #{router.query.id}</h3>
+			<p>Lorem ipsum</p>
+			<Link href="/">
+				<a>Back to blog</a>
+			</Link>
+		</div>
+	);
 }
